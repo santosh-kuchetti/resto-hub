c3bf05d1249eb5cfb5142cf2b8232271bebe41f7
santosh-kuchetti <kuchettisantosh@gmail.com>
2023-11-30 02:19:07 +0530

    commit

--- screens/home/Home.js
+++ screens/home/Home.js
@@ -1,4 +1,4 @@
-import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { FlatList, ScrollView, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native";
 import { StatusBar } from "react-native";
@@ -8,12 +8,23 @@ import Recommended from "./Recommended";
 
 const Home = ({ navigation }) => {
 	return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: "#fff" }}>
-			<ScrollView showsVerticalScrollIndicator={false}>
-				<Details navigation={navigation} />
-				<Offers navigation={navigation} />
-				<Recommended navigation={navigation} />
-			</ScrollView>
+		<SafeAreaView style={{ flex: 1, backgroundColor: "#fff" }}>
+			<FlatList
+				ListHeaderComponent={
+					<>
+						<Details navigation={navigation} />
+						<Offers navigation={navigation} />
+						<Recommended navigation={navigation} />
+					</>
+				}
+				// contentContainerStyle={{
+				// 	paddingBottom: Sizes.fixPadding * 8.0,
+				// 	paddingTop:
+				// 		StatusBar.currentHeight +
+				// 		Sizes.fixPadding * (Platform.OS == "ios" ? 7.0 : 2.0),
+				// }}
+				showsVerticalScrollIndicator={false}
+			/>
 		</SafeAreaView>
 	);
 };
